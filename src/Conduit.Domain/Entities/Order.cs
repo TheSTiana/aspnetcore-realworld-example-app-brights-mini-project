9d0b3f5e6cbd3f6c7784b1a99db55d63cb6acd69
Silje Tiana <silje.tiana@gmail.com>
2023-09-07 14:51:27 +0200

    Created order entity

---
+++ src/Conduit.Domain/Entities/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conduit.Domain.Entities;
+public class Order
+{
+    public int ArticleId { get; set; }
+    public virtual required Article Article { get; set; }
+
+    public int UserId { get; set; }
+    public virtual required User User { get; set; }
+
+    public required bool PyshicalCopy { get; set; }
+
+    [MaxLength(255)]
+    public required string Email { get; set; }
+
+    [MaxLength(255)]
+    public string SnailMail { get; set; } = string.Empty;
+
+}
