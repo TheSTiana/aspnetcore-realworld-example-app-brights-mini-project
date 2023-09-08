4aa271dc3135504d5a022ecf55db881cc5e31508
Silje Tiana <silje.tiana@gmail.com>
2023-09-08 10:35:30 +0200

    Updated migrationfile

--- src/Conduit.Infrastructure/Persistence/Migrations/20230908075951_AddedOrderFunctionality.cs
+++ src/Conduit.Infrastructure/Persistence/Migrations/20230908075951_AddedOrderFunctionality.cs
@@ -2,74 +2,73 @@
 
 #nullable disable
 
-namespace Conduit.Infrastructure.Persistence.Migrations
+namespace Conduit.Infrastructure.Persistence.Migrations;
+
+/// <inheritdoc />
+public partial class AddedOrderFunctionality : Migration
 {
     /// <inheritdoc />
-    public partial class AddedOrderFunctionality : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<int>(
-                name: "DigitalPrice",
-                table: "Articles",
-                type: "integer",
-                nullable: false,
-                defaultValue: 0);
+        migrationBuilder.AddColumn<int>(
+            name: "DigitalPrice",
+            table: "Articles",
+            type: "integer",
+            nullable: false,
+            defaultValue: 0);
 
-            migrationBuilder.AddColumn<int>(
-                name: "PhysicalPrice",
-                table: "Articles",
-                type: "integer",
-                nullable: false,
-                defaultValue: 0);
+        migrationBuilder.AddColumn<int>(
+            name: "PhysicalPrice",
+            table: "Articles",
+            type: "integer",
+            nullable: false,
+            defaultValue: 0);
 
-            migrationBuilder.CreateTable(
-                name: "Order",
-                columns: table => new
-                {
-                    ArticleId = table.Column<int>(type: "integer", nullable: false),
-                    UserId = table.Column<int>(type: "integer", nullable: false),
-                    PyshicalCopy = table.Column<bool>(type: "boolean", nullable: false),
-                    Email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
-                    SnailMail = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Order", x => new { x.ArticleId, x.UserId });
-                    table.ForeignKey(
-                        name: "FK_Order_Articles_ArticleId",
-                        column: x => x.ArticleId,
-                        principalTable: "Articles",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Order_Users_UserId",
-                        column: x => x.UserId,
-                        principalTable: "Users",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                });
+        migrationBuilder.CreateTable(
+            name: "Order",
+            columns: table => new
+            {
+                ArticleId = table.Column<int>(type: "integer", nullable: false),
+                UserId = table.Column<int>(type: "integer", nullable: false),
+                PyshicalCopy = table.Column<bool>(type: "boolean", nullable: false),
+                Email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                SnailMail = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Order", x => new { x.ArticleId, x.UserId });
+                table.ForeignKey(
+                    name: "FK_Order_Articles_ArticleId",
+                    column: x => x.ArticleId,
+                    principalTable: "Articles",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+                table.ForeignKey(
+                    name: "FK_Order_Users_UserId",
+                    column: x => x.UserId,
+                    principalTable: "Users",
+                    principalColumn: "Id",
+                    onDelete: ReferentialAction.Cascade);
+            });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Order_UserId",
-                table: "Order",
-                column: "UserId");
-        }
+        migrationBuilder.CreateIndex(
+            name: "IX_Order_UserId",
+            table: "Order",
+            column: "UserId");
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Order");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Order");
 
-            migrationBuilder.DropColumn(
-                name: "DigitalPrice",
-                table: "Articles");
+        migrationBuilder.DropColumn(
+            name: "DigitalPrice",
+            table: "Articles");
 
-            migrationBuilder.DropColumn(
-                name: "PhysicalPrice",
-                table: "Articles");
-        }
+        migrationBuilder.DropColumn(
+            name: "PhysicalPrice",
+            table: "Articles");
     }
 }
